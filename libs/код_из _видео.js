4128da0ebe579c8cfede19437f0a0ae9b21b623c
NNPCode <kolya680@mail.ru>
2023-02-27 15:55:50 +0300

    mobile

---
+++ libs/код_из _видео.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+   
+    // plagin pageNav
+    $('#header-menu').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+    });
+
+
+
+
+})
+
+
