25e32c2f552b6a4a101faf11d7843312c085de59
NNPCode <kolya680@mail.ru>
2023-02-27 18:02:17 +0300

    header mobile menu

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.querySelector('.header-nav-button'),addEventListener('click', function(){
+    document.querySelector('.header-nav-burger').classList.toggle('header-nav-burger--active')
+});
