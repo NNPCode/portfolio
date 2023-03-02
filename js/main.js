a2aee4724ca5c357f87e85ea2e1ab59383d2b45c
NNPCode <kolya680@mail.ru>
2023-03-02 22:46:01 +0300

    mobile menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-document.querySelector('.mobile-nav').addEventListener('click', function(){
-    this.classList.toggle('mobile-nav--active')
-});
+
+
+    const toggleMenu = document.querySelector('.mobile-nav');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const overlay = document.querySelector('.overlay');
+    const bodyE1 = document.body
+
+    toggleMenu.addEventListener('click', function(){
+
+        this.classList.toggle('mobile-nav--active');
+        mobileMenu.classList.toggle('mobile-menu--active');
+        overlay.classList.toggle('overlay--active');
+        bodyE1.classList.toggle('noscroll');
+    })
+
+    mobileMenu.addEventListener('click', function(){
+
+        this.classList.remove('mobile-menu--active');
+        toggleMenu.classList.remove('mobile-nav--active');
+        overlay.classList.remove('overlay--active');
+        bodyE1.classList.remove('noscroll');
+    })
+
+    window.addEventListener('resize', function(){
+        
+        toggleMenu.classList.remove('mobile-nav--active');
+        mobileMenu.classList.remove('mobile-menu--active');
+        overlay.classList.remove('overlay--active');
+        bodyE1.classList.remove('noscroll');
+    })
+
+
+// document.querySelector('.mobile-nav').addEventListener('click', function(){
+//     this.classList.toggle('mobile-nav--active')
+//     document.querySelector('.mobile-menu').classList.toggle('mobile-menu--active')
+//     document.querySelector('.overlay').classList.toggle('overlay--active')
+// });
+
+
