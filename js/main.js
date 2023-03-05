3f455186d11965d55a9a2035e8a5e1b9b7810f5b
NNPCode <kolya680@mail.ru>
2023-03-05 23:08:56 +0300

    contact form

--- js/main.js
+++ js/main.js
@@ -42,11 +42,86 @@ $(document).ready(function(){
     $('#header-top-nav').onePageNav({
         currentClass: 'active',
         changeHash: false,
-        scrollSpeed: 750,
-        scrollThreshold: 0.8,
+        scrollSpeed: 500,
+        scrollThreshold: 0.5,
         filter: '',
         easing: 'swing',
     });
+
+    // Form Placeholder
+
+    const formItems = document.querySelectorAll('.form-field');
+
+    for(let item of formItems){
+        const thisParent = item.closest('.form-item');
+        const thisPlaceholder = thisParent.querySelector('.form-placeholder');
+        // Если input в фокусе
+        item.addEventListener('focus', function(){
+            thisPlaceholder.classList.add('form-placeholder--active');
+        });
+
+        // Если input теряет фокус
+        item.addEventListener('blur', function(){
+            
+            if(item.value.length > 0){
+                thisPlaceholder.classList.add('form-placeholder--active');
+            }
+             else{
+                thisPlaceholder.classList.remove('form-placeholder--active')
+             }   
+        })
+    }
+
+      //FORM VALIDATE
+	$('.contact-form').validate({
+		rules: {
+			email: {
+				required: true,
+				email: true
+			},
+			
+			message: {
+				required: true
+			}
+		},
+		messages: {
+			email: {
+				required: 'Введите E-mail',
+				email: 'Неверный формат email'
+			},
+			
+			message: {
+				required: 'Поле не должно быть пустым'
+			}
+		},
+		submitHandler: function (form) {
+			ajaxFormSubmit();
+		}
+
+	});
+
+	//*************************************************** */
+	// Функция AJAX запрса на сервер
+
+	function ajaxFormSubmit() {
+
+		let string = $(".contact-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+		//Формируем ajax запрос
+		$.ajax({
+			type: "POST", // Тип запроса - POST
+			url: "php/mail.php", // Куда отправляем запрос
+			data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+			// Функция если все прошло успешно
+			success: function (html) {
+				$(".contact-form").slideUp(800);
+				$('#answer').html(html);
+			}
+		});
+		// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+		return false;
+	}
 
 })    
 
